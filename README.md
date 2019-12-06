e06cf386cdde8395edc3aefde5980ada576ec482
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2019-12-06 13:14:00 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Users can:
+  search
+    by: genre, country, avg_vote (strings, case insensitive)
+
+Endpoint: /movie
+
+search:
+  country (use .includes())
+  avg_vote ( >= supplied number)
+
+response = [...full movie entries]
---
+++ server.js
@@ -0,0 +1,49 @@
+/* eslint-disable quotes */
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const MOVIEDEX = require("./movies.js");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "DENIED!" });
+  }
+  next();
+});
+
+app.get("/movie", function handleGenreTypes(req, res) {
+  const { genre, country, avg_vote } = req.query;
+  let response = MOVIEDEX;
+
+  if (genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+  if (country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+  if (avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(avg_vote)
+    );
+  }
+  res.json(response);
+});
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
